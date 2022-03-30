436e2a0bc1e7a55589d853aadfe73d55565d1bb8
austinmula <maustin64.ma@gmail.com>
2022-03-31 00:16:53 +0300

    Simple Arduino Code

---
+++ ini/sketch_mar30a/sketch_mar30a.ino
@@ -0,0 +1,32 @@
+#include <ArduinoJson.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  DynamicJsonDocument doc(300);
+  //  StaticJsonDocument <300> doc;   //Declaring static JSON buffer
+
+  doc["temperature"] = getTemperatureValue();
+  doc["humidity"] =  getHumidityValue();
+  doc["WL"] =  getHumidityValue();
+
+  serializeJson(doc, Serial);
+  Serial.println();
+  delay(60000);
+}
+
+int getTemperatureValue() {
+  return random(21, 26);
+}
+
+
+int getHumidityValue() {
+  return random(44, 60);
+}
+
+int getWLevel() {
+  return random(35, 60);
+}
